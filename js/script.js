02ae3d1fb93e70c351cfc9026965f93a881ca943
Antwon Frager Jr <Antwon.frager@gmail.com>
2023-12-02 20:17:59 -0500

    Encapsulate player functionality in gameCanvas, improved naming conventions.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-const canvasWidth = 600;
-const canvasHeight = 400;
-let player = 0;
-let playerYPosition = 100;
+const canvasWidth = 800;
+const canvasHeight = 600;
+const playerYPosition = 100;
+
 
 const gameCanvas = {
     canvas: document.createElement('canvas'),
+    player: null,
+
     start: function() {
         this.canvas.width = canvasWidth;
         this.canvas.height = canvasHeight;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.player = new Player(30, 30, 10);
+        this.player.draw();
     } 
 };
-function startGame() {
-    gameCanvas.start();
-    
-    player = new createPlayer(30, 30, 10);
-}
 
-function createPlayer(radius, xPos) {
+
+function Player(radius, xPos) {
     this.radius = radius
     this.x = xPos;
     this.y = playerYPosition;
+
 // Drawing circle using "ctx" 2D rendering context of the canvas
-    ctx = gameCanvas.context
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.closePath();
+    this.draw = function() {
+        const ctx = gameCanvas.context;
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+    };
+}
+
+function startGame() {
+    gameCanvas.start();
+    
 }
 
 startGame();
