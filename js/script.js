69ad16f02988264669a289217a2be5d0fb50de8d
Antwon Frager Jr <Antwon.frager@gmail.com>
2023-12-04 22:02:32 -0500

    added the score and collision logic, working on the game to restart

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const canvasWidth = 800;
 const canvasHeight = 600;
 const playerYPosition  = canvasHeight - 30;
+let score = 0;
 
 const keys = {
     space: false,
@@ -9,6 +10,7 @@ const keys = {
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function createBlock() {
     const width = randomNumber(50, 100);
     const height = randomNumber(50, 100);
@@ -18,7 +20,7 @@ function createBlock() {
 }
 
 document.body.addEventListener("keydown", function(event) {
-     if (event.code === "Space") {
+    if (event.code === "Space") {
         keys.space = true;
     }
 });
@@ -29,13 +31,41 @@ document.body.addEventListener("keyup", function (event) {
     }
 });
 
-
-
 const gameCanvas = {
     canvas: document.getElementById('gameCanvas'),
     player: null,
+    block: null,
     fallSpeed: 2,
     updateInterval: null,
+    gameOver: false,
+
+    stopCanvas: function () {
+        console.log("GAME OVER - Better Luck Next Time");
+        clearInterval(this.updateInterval);
+    },
+
+    detectCollision: function () {
+        const player = this.player;
+        const block = this.block;
+
+        if (
+            player.x + player.radius >= block.x &&
+            player.x - player.radius <= block.x + block.width &&
+            player.y + player.radius >= block.y && 
+            player.y - player.radius <= block.y + block.height
+        ) {
+            console.log("GAME OVER - Better Luck Next Time");
+            clearInterval(this.updateInterval);
+            this.end();
+        }
+    },
+
+    drawScore: function () {
+        const ctx = this.context;
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText("Score: " + score, 10, 20);
+    },
 
     start: function () {
         this.canvas.width = canvasWidth;
@@ -45,27 +75,77 @@ const gameCanvas = {
         this.player = new Player(30, 30, 10, playerYPosition);
         this.player.draw();
 
+        this.block = createBlock();
+
         this.updateInterval = setInterval(this.updateCanvas.bind(this), 1000 / 60);
     },
 
-    updateCanvas: function () {
+    end: function () {
+        console.log("GAME OVER - Better Luck Next Time");
+        this.gameOver = true;
         this.clearCanvas();
-        this.player.move();
-        this.player.draw();
-
-        block.draw();
-        block.attackPlayer();
-
-        if(keys.space) {
-            this.player.jump();
-        }
+        this.drawGameOverMessage();
+        this.drawRestartMessage();
+        this.addRestartSpaceBarListener();
+        score = 0;
+        this.player = null;
+        this.block = null;
     },
 
-    clearCanvas: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+    drawRestartMessage: function () {
+       if (this.gameOver) { 
+            const ctx = this.context;
+            ctx.font = "20px Arial";
+            ctx.fillStyle = "black";
+            ctx.fillText("Press space bar to start", canvasWidth / 2 - 130, canvasHeight / 2 + 50);
+       }
+    },
 
-    
+    addRestartSpaceBarListener: function () {
+        const restartHandler = (event) => {
+            if (event.code === "Space") {
+                document.removeEventListener("keydown", restartHandler);
+                startGame();
+            }
+        };
+
+        document.addEventListener("keydown", restartHandler);
+    },
+
+    drawGameOverMessage: function () {
+        const ctx = this.context;
+        ctx.font = "40px Arial";
+        ctx.fillStyle = "red";
+        ctx.fillText("GAME OVER", canvasWidth / 2 - 100, canvasHeight / 2 - 20);
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText("Score: " + score, canvasWidth / 2 - 30, canvasHeight / 2 + 20);
+    },
+
+    updateCanvas: function () {
+        if (this.gameOver) {
+            return;
+        }
+            this.clearCanvas();
+            this.player.move();
+            this.player.draw();
+
+            this.block.draw();
+            this.block.attackPlayer();
+
+            this.detectCollision();
+
+            this.drawScore();
+
+            if(keys.space) {
+                this.player.jump();
+            }
+    },
+
+    clearCanvas: function () { 
+        this.context.beginPath();
+        this.context.clearRect(0, 0, 800, 600);
+    },  
 };
 
 class Player {
@@ -73,9 +153,9 @@ class Player {
         this.radius = radius;
         this.x = xPos;
         this.y = playerYPos;
-        this.fallSpeed = 10;
+        this.fallSpeed = 5;
         this.isJumping = false;
-        this.jumpSpeed = 7.5;
+        this.jumpSpeed = 10;
         this.jumpHeight = 10;
         this.jumpDistance = 0;
     }
@@ -92,16 +172,16 @@ class Player {
     move() {
         if (this.isJumping) {
             if (this.jumpDistance < this.jumpHeight) {
-            this.y -= this.jumpSpeed;
-            this.jumpDistance += this.jumpSpeed;
-        } else {
-            this.isJumping = false;
-            this.jumpDistance = 0;
-         }
+                this.y -= this.jumpSpeed;
+                this.jumpDistance += this.jumpSpeed;
+            } else {
+                this.isJumping = false;
+                this.jumpDistance = 0;
+            }
         } else {
             this.y += this.fallSpeed;
             this.stopPlayer();
-      }
+        }
     }
 
     stopPlayer() {
@@ -112,16 +192,17 @@ class Player {
     }
 
     jump() {
-        if(!this.isJumping) {
+        if (!this.isJumping) {
             this.isJumping = true;
         }
     }
+
     resetJump() {
         this.isJumping = false;
     }
-};
+}
 
-class Block{
+class Block {
     constructor(width, height, speed) {
         this.width = width;
         this.height = height;
@@ -130,15 +211,18 @@ class Block{
         this.y = canvasHeight - this.height;
     }
 
-     returnToAttackPosition() {
-        this.width = randomNumber(50, 100);
-        this.height = randomNumber(50, 100);
-        this.speed = randomNumber(1, 5)
-        this.x = canvasWidth;
-        this.y = canvasHeight - this.height;
-     }
+    returnToAttackPosition() {
+        if (this.x < 0) {
+            this.width = randomNumber(50, 100);
+            this.height = randomNumber(50, 250);
+            this.speed = randomNumber(5, 10);
+            score++;
+            this.x = canvasWidth;
+            this.y = canvasHeight - this.height;
+        }
+    }
 
-     attackPlayer() {
+    attackPlayer() {
         const player = gameCanvas.player;
         if (
             player.x + player.radius >= this.x &&
@@ -148,8 +232,10 @@ class Block{
         ) { 
             console.log("Player Hit");
             this.returnToAttackPosition();
+            gameCanvas.stopCanvas();
         }
     }
+
     draw() {
         const ctx = gameCanvas.context;
         ctx.fillStyle = "white";
@@ -162,13 +248,13 @@ class Block{
         this.x -= this.speed;
     }
 }
-let block = createBlock();
-
-
-
 
 function startGame() {
     gameCanvas.start();
+}
+
+function endGame() {
+    gameCanvas.end();
 }
 
 startGame();
